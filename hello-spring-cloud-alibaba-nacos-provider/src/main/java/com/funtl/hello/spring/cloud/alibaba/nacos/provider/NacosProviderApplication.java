10674b563131e6d1f4ea25bc938ad033305bb5db
gx1360414553 <1360414553@qq.com>
2019-08-20 18:00:54 +0800

    初始化项目

---
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/NacosProviderApplication.java
@@ -0,0 +1,30 @@
+package com.funtl.hello.spring.cloud.alibaba.nacos.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 高雄
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2019年08月20日 17:46:00
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class, args);
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable String message) {
+            return "Hello Nacos Discovery " + message;
+        }
+    }
+}
