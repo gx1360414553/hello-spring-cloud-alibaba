b58ce8d22c16d58c7456882f753bb3b96a0cf810
gx1360414553 <1360414553@qq.com>
2019-08-21 15:53:38 +0800

    spring cloud geteway 全局过滤器 OK!

--- hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,6 +1,8 @@
 package com.funtl.hello.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,16 @@ public class NacosProviderController {
     @Value("${server.port}")
     private String port;
 
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable String message) {
         return "Hello Nacos Discovery " + message + "I am from " + port;
+    }
+    @GetMapping("/hi")
+    public String hi(){
+        String name = applicationContext.getEnvironment().getProperty("user.name");
+        return "name:" + name;
     }
 }
